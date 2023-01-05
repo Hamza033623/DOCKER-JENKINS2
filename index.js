831fc639f528663245ecd0042ddac1a2c4105c15
Hamza033623 <116195255+Hamza033623@users.noreply.github.com>
2023-01-05 23:51:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("This is my express app I WANT TO BE YOURS be with me for the whole liffeeee");
+  res.send("This is my express app I WANT TO BE YOURS be with me forr the whole liffeeee");
 });
 
 app.get("/me", (req, res) => {
-  res.send("Hi I am Laith hamza");
+  res.send("Hi I am Laith hamzsa");
 });
 
 app.listen(5000, () => {
